2d4946c3ffb8cdffd398ec70a201c2353920102c
A01375667 <A01375667@itesm.mx>
2017-03-27 21:28:48 -0600

    //

--- core/src/mx/e5/madmom/MadMom.java
+++ core/src/mx/e5/madmom/MadMom.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -20,7 +21,6 @@ public class MadMom extends Game
 		assetManager = new AssetManager();
 	}
 
-
 	@Override
 	public void create () {
 		// Pone la pantalla inicial (Splash)
@@ -30,7 +30,6 @@ public class MadMom extends Game
 	public AssetManager getAssetManager() {
 		return assetManager;
 	}
-
 
 	@Override
 	public void dispose(){
